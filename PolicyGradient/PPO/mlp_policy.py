3ec41b128af1f557cc68c4e76dd1bce5c2ed84e9
Jiyao Li <jiyao.li@aggiemail.usu.edu>
2021-10-05 11:58:06 -0600

    change the interface more flexible

--- PolicyGradient/PPO/mlp_policy.py
+++ PolicyGradient/PPO/mlp_policy.py
@@ -8,9 +8,9 @@ import numpy as np
 class MLP_Net(nn.Module):
     def __init__(self, input_dims, n_actions, eta, tb_writer, chkpt_dir='checkpoints'):
         super(MLP_Net, self).__init__()
-        self.fc1 = nn.Linear(80*80, 256)
+        self.fc1 = nn.Linear(input_dims, 256)
         # self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 2)
+        self.fc3 = nn.Linear(256, n_actions)
 
         self.optimizer = optim.RMSprop(self.parameters(), lr=eta)    #0.0001
         # self.optimizer = optim.SGD(self.__policy.parameters(), lr=0.0001)  # 0.01 for method2, 3, online learn
